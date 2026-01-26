6036cad37a39715e1a7fc0d1ec4100bc3f582add
Jake Tuero <jaketuero94@gmail.com>
2026-01-26 02:48:14 -0700

    fix passthrough for libtorch env

--- ports/libpolicyts/portfile.cmake
+++ ports/libpolicyts/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS ${FEATURE_OPTIONS}
+    OPTIONS ${FEATURE_OPTIONS} "-DCMAKE_PREFIX_PATH=$ENV{LIBTORCH_ROOT}"
 )
 vcpkg_cmake_install()
 
